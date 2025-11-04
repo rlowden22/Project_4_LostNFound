18fc42e98d6c38103da80122cdc55689be82ca8b
Puneet Puri <puneet.puri999@gmail.com>
2025-11-04 13:41:19 -0500

    HomeScreen will now only show recent lost items

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Item from "../components/Item";
 import items from "../items";
 import "../styles/screens/HomeScreen.css";
 
 const HomeScreen = () => {
+  // Get 4 most recent items based on dateFound
+  const recentItems = useMemo(() => {
+    return [...items]
+      .sort((a, b) => new Date(b.dateFound) - new Date(a.dateFound))
+      .slice(0, 4);
+  }, []);
+
   return (
     <div className="home-screen">
       <Container>
-        <h1 className="home-title">Lost Items</h1>
+        <h1 className="home-title">Recent Lost Items</h1>
         <Row>
-          {items.map((item) => (
+          {recentItems.map((item) => (
             <Col key={item._id} sm={12} md={6} lg={4} xl={3}>
               <Item item={item} />
             </Col>
